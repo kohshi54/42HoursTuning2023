4ef50f40973ccef29e92e814b1a8244b42ffbb6b
Ubuntu <azureuser@env-elephant.ebo3uwt1ekruxkly0wombn0foc.lx.internal.cloudapp.net>
2023-06-17 07:38:07 +0000

    bench#5score439 Results per API:
        - POST /api/v1/session
          ✓ requests: 159, success: 39, fail: 0, timeout: 120
          ✓ rps: 0.65
    
        - GET /api/v1/users/user-icon/{userIconId}
          ✓ requests: 163, success: 49, fail: 0, timeout: 114
          ✓ rps: 0.82
    
        - GET /api/v1/users
          ✓ requests: 146, success: 0, fail: 0, timeout: 146
          ✓ rps: 0
    
        - GET /api/v1/users/search
          ✓ requests: 147, success: 0, fail: 0, timeout: 147
          ✓ rps: 0
    
        - POST /api/v1/match-groups
          ✓ requests: 146, success: 0, fail: 0, timeout: 146
          ✓ rps: 0
    
        - GET /api/v1/match-groups/members/{userId}
          ✓ requests: 146, success: 0, fail: 0, timeout: 146
          ✓ rps: 0
    
        ================================================================
            Congratulations! All Scoring Process Successfully Done!!
    
            Score:                 439
    
            Total requests:        907
              Success:              88
              Fail:                  0
              Timeout:             819
        ================================================================

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -1,1 +1,3 @@
 ALTER TABLE department_role_member ADD INDEX user_id_idx(user_id);
+ALTER TABLE match_group_member ADD INDEX user_id_idx(user_id);
+ALTER TABLE department_role_member ADD INDEX role_id_idx(role_id);
