95f4ce9bc472af5faa299273f4e559fd30290822
Ubuntu <azureuser@env-elephant.ebo3uwt1ekruxkly0wombn0foc.lx.internal.cloudapp.net>
2023-06-17 11:07:55 +0000

    bench#10score831 real:0 connection=1000 Results per API:
        - POST /api/v1/session
          ✓ requests: 172, success: 76, fail: 0, timeout: 96
          ✓ rps: 1.27
    
        - GET /api/v1/users/user-icon/{userIconId}
          ✓ requests: 179, success: 71, fail: 0, timeout: 108
          ✓ rps: 1.18
    
        - GET /api/v1/users
          ✓ requests: 146, success: 0, fail: 0, timeout: 146
          ✓ rps: 0
    
        - GET /api/v1/users/search
          ✓ requests: 146, success: 0, fail: 0, timeout: 146
          ✓ rps: 0
    
        - POST /api/v1/match-groups
          ✓ requests: 146, success: 0, fail: 0, timeout: 146
          ✓ rps: 0
    
        - GET /api/v1/match-groups/members/{userId}
          ✓ requests: 146, success: 0, fail: 0, timeout: 146
          ✓ rps: 0
    
        ================================================================
            Congratulations! All Scoring Process Successfully Done!!
    
            Score:                 831
    
            Total requests:        935
              Success:             147
              Fail:                  0
              Timeout:             788
        ================================================================

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -1,10 +1,9 @@
 ALTER TABLE department_role_member ADD INDEX user_id_idx(user_id);
 ALTER TABLE match_group_member ADD INDEX user_id_idx(user_id);
 ALTER TABLE department_role_member ADD INDEX role_id_idx(role_id);
-ALTER TABLE user ADD FULLTEXT INDEX ngram_idx (goal) WITH PARSER ngram;
-
-ALTER TABLE user ADD FULLTEXT INDEX ngram_idx (user_name) WITH PARSER ngram;
-ALTER TABLE user ADD FULLTEXT INDEX ngram_idx (mail) WITH PARSER ngram;
-ALTER TABLE user ADD FULLTEXT INDEX ngram_idx (kana) WITH PARSER ngram;
+ALTER TABLE user ADD FULLTEXT INDEX ngram_goal_idx (goal) WITH PARSER ngram;
+ALTER TABLE user ADD FULLTEXT INDEX ngram_user_name_idx (user_name) WITH PARSER ngram;
+ALTER TABLE user ADD FULLTEXT INDEX ngram_mail_idx (mail) WITH PARSER ngram;
+ALTER TABLE user ADD FULLTEXT INDEX ngram_kana_idx (kana) WITH PARSER ngram;
 ALTER TABLE user ADD INDEX mail_idx(mail);
 ALTER TABLE user ADD INDEX entry_date_kana_idx(entry_date, kana);
