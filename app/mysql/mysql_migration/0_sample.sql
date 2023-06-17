e1466987c722fc277eee0cfeb28517cc0dffd321
Ubuntu <azureuser@env-elephant.ebo3uwt1ekruxkly0wombn0foc.lx.internal.cloudapp.net>
2023-06-17 09:57:18 +0000

    benc#8score845 make mail_idx Results per API:
        - POST /api/v1/session
          ✓ requests: 185, success: 78, fail: 0, timeout: 107
          ✓ rps: 1.3
    
        - GET /api/v1/users/user-icon/{userIconId}
          ✓ requests: 178, success: 65, fail: 0, timeout: 113
          ✓ rps: 1.08
    
        - GET /api/v1/users
          ✓ requests: 146, success: 0, fail: 0, timeout: 146
          ✓ rps: 0
    
        - GET /api/v1/users/search
          ✓ requests: 146, success: 0, fail: 0, timeout: 146
          ✓ rps: 0
    
        - POST /api/v1/match-groups
          ✓ requests: 146, success: 0, fail: 0, timeout: 146
          ✓ rps: 0
    
        - GET /api/v1/match-groups/members/{userId}
          ✓ requests: 146, success: 0, fail: 0, timeout: 146
          ✓ rps: 0
    
        ================================================================
            Congratulations! All Scoring Process Successfully Done!!
    
            Score:                 845
    
            Total requests:        947
              Success:             143
              Fail:                  0
              Timeout:             804
        ================================================================

--- app/mysql/mysql_migration/0_sample.sql
+++ app/mysql/mysql_migration/0_sample.sql
@@ -2,3 +2,8 @@ ALTER TABLE department_role_member ADD INDEX user_id_idx(user_id);
 ALTER TABLE match_group_member ADD INDEX user_id_idx(user_id);
 ALTER TABLE department_role_member ADD INDEX role_id_idx(role_id);
 ALTER TABLE user ADD FULLTEXT INDEX ngram_idx (goal) WITH PARSER ngram;
+
+ALTER TABLE user ADD FULLTEXT INDEX ngram_idx (user_name) WITH PARSER ngram;
+ALTER TABLE user ADD FULLTEXT INDEX ngram_idx (mail) WITH PARSER ngram;
+ALTER TABLE user ADD FULLTEXT INDEX ngram_idx (kana) WITH PARSER ngram;
+ALTER TABLE user ADD INDEX mail_idx(mail);
